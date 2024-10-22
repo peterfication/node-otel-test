a3e3fb3f53f270b2a54282c720b985f4be784644
Peter Morgenstern <peter.morgenstern@prenode.de>
2024-10-22 11:42:33 +0200

    Add OpenTelemetry test

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log('Hello World!');
+import { trace } from "@opentelemetry/api";
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import {
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-node";
+
+console.log("Configuring SDK");
+
+const sdk = new NodeSDK({
+  traceExporter: new ConsoleSpanExporter(),
+  spanProcessors: [new SimpleSpanProcessor(new ConsoleSpanExporter())],
+});
+
+console.log("Starting SDK");
+sdk.start();
+console.log("Starting trace");
+
+const tracer = trace.getTracer("example-tracer");
+tracer.startActiveSpan("expensive-query", (span) => {
+  console.log("Doing cool stuff ...");
+  span.end();
+
+  console.log("Trace ended");
+  console.log("");
+});
