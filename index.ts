9d4d232218b85959112b9094cc08ca4df8ada982
Peter Morgenstern <peter.morgenstern@prenode.de>
2024-10-22 13:00:46 +0200

    Create CustomConsoleSpanExporter

--- index.ts
+++ index.ts
@@ -1,15 +1,34 @@
 import { trace } from "@opentelemetry/api";
+import { ExportResult } from "@opentelemetry/core";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import {
   ConsoleSpanExporter,
+  ReadableSpan,
   SimpleSpanProcessor,
 } from "@opentelemetry/sdk-trace-node";
 
+class CustomConsoleSpanExporter extends ConsoleSpanExporter {
+  override export(
+    spans: ReadableSpan[],
+    _resultCallback: (result: ExportResult) => void,
+  ): void {
+    spans.forEach((span) => {
+      console.log(
+        "OTEL",
+        span.instrumentationLibrary.name,
+        JSON.stringify(span.attributes),
+      );
+    });
+  }
+}
+
 console.log("Configuring SDK");
 
+const exporter = new CustomConsoleSpanExporter();
+
 const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
-  spanProcessors: [new SimpleSpanProcessor(new ConsoleSpanExporter())],
+  traceExporter: exporter,
+  spanProcessors: [new SimpleSpanProcessor(exporter)],
 });
 
 console.log("Starting SDK");
@@ -19,6 +38,7 @@ console.log("Starting trace");
 const tracer = trace.getTracer("example-tracer");
 tracer.startActiveSpan("expensive-query", (span) => {
   console.log("Doing cool stuff ...");
+
   span.end();
 
   console.log("Trace ended");
